a3cc73f4fb0f8a980628798c13e7e746b3352ca8
n1bil <nabil.nadzor@gmail.com>
2024-04-13 14:50:03 +0200

    Sidebar

--- frontend/src/components/SmallSidebar.tsx
+++ frontend/src/components/SmallSidebar.tsx
@@ -1,14 +1,54 @@
-import Wrapper from '../assets/wrappers/SmallSidebar'
-import { useDashboardContext } from '../pages/DashboardLayout'
+import { FaTimes } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { useDashboardContext } from "../pages/DashboardLayout";
+import Logo from "./Logo";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
 
 const SmallSidebar = () => {
-  const data = useDashboardContext();
-  console.log(data);
-  
+    const { showSidebar, toggleSideBar } = useDashboardContext();
 
-  return (
-    <Wrapper>SmallSidebar</Wrapper>
-  )
-}
+    return (
+        <Wrapper>
+            <div
+                className={
+                    showSidebar
+                        ? "sidebar-container show-sidebar"
+                        : "sidebar-container"
+                }
+            >
+                <div className="content">
+                    <button
+                        type="button"
+                        className="close-btn"
+                        onClick={toggleSideBar}
+                    >
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <div className="nav-links">
+                        {links.map((link) => {
+                            const { text, path, icon } = link;
+                            return (
+                                <NavLink
+                                    to={path}
+                                    key={text}
+                                    className="nav-link"
+                                    onClick={toggleSideBar}
+                                    end
+                                >
+                                    <span className="icon">{icon}</span>
+                                    {text}
+                                </NavLink>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
+    );
+};
 
-export default SmallSidebar
+export default SmallSidebar;
