43e806216248a73b6cf9b7cec0a1d501db2be462
n1bil <nabil.nadzor@gmail.com>
2024-04-18 20:03:53 +0200

    All jobs structure

--- frontend/src/pages/AllJobs.tsx
+++ frontend/src/pages/AllJobs.tsx
@@ -1,7 +1,50 @@
-const AllJobs = () => {
-  return (
-    <div>AllJobs</div>
-  )
+import { JobsContainer, SearchContainer } from "../components";
+import customFetch from "../utils/customFetch";
+import { useLoaderData } from "react-router-dom";
+import { useContext, createContext } from "react";
+import { CustomAxiosError, handleError } from "../utils/CustomError";
+
+interface Job {
+    id: string;
+    company: string;
+    position: string;
+    jobStatus: string;
+    jobType: string;
+    jobLocation: string;
+    createdBy: string;
+    createdAt: string;
+    updatedAt: string;
 }
 
-export default AllJobs
+interface AllJobsContextType {
+    jobs: Job[];
+}
+
+export const loader = async () => {
+    try {
+        const { data } = await customFetch.get("/jobs");
+        return { data };
+    } catch (error) {
+        handleError(error as CustomAxiosError);
+        return error;
+    }
+};
+
+const AllJobsContext = createContext<AllJobsContextType>({
+    jobs: []
+});
+
+const AllJobs = () => {
+    const { data } = useLoaderData() as { data: Job[] };
+    
+    return (
+        <AllJobsContext.Provider value={{ jobs: data }}>
+            <SearchContainer />
+            <JobsContainer />
+        </AllJobsContext.Provider>
+    );
+};
+
+export const useAllJobsContext = () => useContext(AllJobsContext);
+
+export default AllJobs;
