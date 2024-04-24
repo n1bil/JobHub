5f83ae7a30de192477e94d841b55b0054224dd6d
n1bil <nabil.nadzor@gmail.com>
2024-04-24 18:21:28 +0200

    Localhost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:8080/api",
+                target: "https://backend-71vg.onrender.com/api",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
