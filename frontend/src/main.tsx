9532d16120f5c3cc1e8b20c49dedb4b0db8823eb
n1bil <nabil.nadzor@gmail.com>
2024-04-25 14:26:11 +0200

    Data

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,11 @@ import "./index.css";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
+fetch('/api/jobs') // Вместо прямого обращения к бэкенду
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error fetching data:', error));
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
