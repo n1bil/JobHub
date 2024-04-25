3a77ecdc9ecdccdeee5ac869bb80ec169c92dea7
n1bil <nabil.nadzor@gmail.com>
2024-04-25 14:32:28 +0200

    Data

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-fetch('/api/jobs') // Вместо прямого обращения к бэкенду
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error fetching data:', error));
+fetch("/api/v1/jobs")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error fetching data:", error));
+
+    fetch("https://backend-6hkg.onrender.com/api/v1/jobs")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error fetching data:", error));
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
