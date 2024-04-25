f39e0bd4d904f32a3fad52354780edcfac65f3fa
n1bil <nabil.nadzor@gmail.com>
2024-04-25 14:16:32 +0200

    Proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:8080/api",
+                target: "https://backend-6hkg.onrender.com",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
