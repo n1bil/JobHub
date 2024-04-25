36dfa713fcc7b046ef374f954637aaab352eedc4
n1bil <nabil.nadzor@gmail.com>
2024-04-25 14:34:47 +0200

    Data

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,15 @@ import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
-fetch("/api/v1/jobs")
+// fetch("/api/v1/jobs")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((error) => console.error("Error fetching data:", error));
+
+fetch("https://backend-6hkg.onrender.com/api/v1/jobs")
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.error("Error fetching data:", error));
-
-    fetch("https://backend-6hkg.onrender.com/api/v1/jobs")
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error("Error fetching data:", error));
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
