8d4cd02079bbc92df36c268e9572d4e2ca380c9e
n1bil <nabil.nadzor@gmail.com>
2024-04-26 09:07:07 +0200

    Proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "https://backend-6hkg.onrender.com/api/v1/auth/login",
+                target: "http://localhost:8080/api",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
