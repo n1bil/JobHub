a34752ebf3d88d4a4255593fdc5a7b33604e10d3
n1bil <nabil.nadzor@gmail.com>
2024-04-28 11:21:43 +0200

    Logo

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Logo } from './Logo';
 export { default as FormRow } from './FormRow';
 export { default as BigSidebar } from './BigSidebar';
 export { default as SmallSidebar } from './SmallSidebar';
